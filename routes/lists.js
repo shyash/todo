67112183f754e318c57ef17ee0d77838ca9b28a0
shyash <shyash222@gmail.com>
2019-05-18 16:59:54 +0530

    add new lists route

---
+++ routes/lists.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router 	= express.Router();
+const db = require("../models")
+const helpers = require("../helpers/lists")
+
+router.route("/")
+				.get(helpers.getLists)
+				.post(helpers.createList)
+
+router.route("/:listId")
+				.get(helpers.getList)
+				.put(helpers.updateList)
+				.delete(helpers.deleteList)
+
+router.route("/:listId/todos")
+				.get(helpers.getTodos)
+				.post(helpers.createTodo)
+
+router.route("/:listId/todos/:todoId")
+				.put(helpers.updateTodo) 
+				.delete(helpers.deleteTodo) 
+
+router.route("/:listId/collab/:collabId")
+				.post(helpers.addCollab)
+				.delete(helpers.deleteCollab)
+
+module.exports = router
