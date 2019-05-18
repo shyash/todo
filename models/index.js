b1a024001c5248dd948fe4b059286732bdc7c2f9
shyash <shyash222@gmail.com>
2019-05-18 14:44:58 +0530

    single todo list version

--- models/index.js
+++ models/index.js
@@ -4,4 +4,4 @@ mongoose.set('debug', true)
 mongoose.connection.openUri(DBURI, {useNewUrlParser:  true, useFindAndModify: false})
 mongoose.Promise = Promise
 
-module.exports.Todo = require("./todo")
+module.exports.Todo = require("./todo") 
