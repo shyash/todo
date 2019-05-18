41417fa6159854420c3519972c64958b44343a57
shyash <shyash222@gmail.com>
2019-05-18 16:57:52 +0530

    update model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const DBURI = process.env.DBURI || 'mongodb://localhost/todo';
+const DBURI = process.env.DBURI || 'mongodb://localhost/todo_lists';
 mongoose.set('debug', true)
 mongoose.connection.openUri(DBURI, {useNewUrlParser:  true, useFindAndModify: false})
 mongoose.Promise = Promise
 
-module.exports.Todo = require("./todo") 
+module.exports.List = require("./list") 
