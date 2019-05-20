a2b725d2522145623d31fa30fd37a6ef33f3ca6b
shyash <shyash222@gmail.com>
2019-05-20 22:39:36 +0530

    update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require("express");
 const app 	= express();
 const bodyParser = require("body-parser");
 const flash = require('connect-flash');
-const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
@@ -40,12 +38,14 @@ app.get("/",function(req,res) {
 	res.render("index");
 })
 app.use("/api/lists", listRoutes)
+
+
 app.get("/register",function(req,res){
   res.render("signup");
 });
 
 app.post("/register",function(req,res){
-  var newuser = new User({username:req.body.username,mail:req.body.mail});
+  let newuser = new User({username:req.body.username,mail:req.body.mail});
   User.register(newuser,req.body.password,function(err,user){
     if(err){
       req.flash("error",err.message);
@@ -66,54 +66,11 @@ app.post("/register",function(req,res){
 app.get("/login",function(req,res){
   res.render("login");
 })
-app.get("/forgot",function(req,res){
-  res.render("forgot");
-})
-app.post("/forgot",function(req,res){
-  User.find({},function(err,users) {
-    let check=0,mailID;
-    for (let i = 0; i < users.length; i++) {
-     if(users[i].username == req.body.username){ 
-      check++;
-       mailID = users[i].mail;
-      let content = "The password you had set was '<strong>"+users[i].p2+"</strong>' <br> <a href='https://attendancemanager.herokuapp.com/login' target='_blank'>login here</a>";
-
-let transporter = nodemailer.createTransport(smtpTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
-  auth: {
-    user: 'shyash222@gmail.com',
-    pass: ' '
-  }, tls: {
-        rejectUnauthorized: false
-    }
-}));
-let mailOptions = {
-  from: '"todo" <shyash222@gmail.com>',
-  to: users[i].mail,
-  subject: 'forgot password --todo app',
-  html: content
-};
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-  
- 
-  } 
-});   
-    }  
-    }
-
-    if(check!=0){req.flash("success","an email has been sent to "+ mailID + " with password");res.redirect("back");}
-    else{req.flash("error","no user found with username " + req.body.username + " !");res.redirect("back");}
-  });
-  
-});
 
 app.post("/login",passport.authenticate("local",{successRedirect:"/",failureRedirect:"/login"}),function(req,res){
  if (err) {console.log(err)}
 });
+
 app.get("/logout",isLoggedIn,function(req,res){
   req.logout();
   req.flash("success","logged out successfully");
